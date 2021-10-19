085c0fcc082c600652850f62419ebe007a969b94
Siratee K <sirateek@gmail.com>
2021-10-19 23:47:52 +0700

    Add Usage: home [OPTIONS] COMMAND [ARGS]...
    Options:
      --help  Show this message and exit.
    
    Commands:
      br
      hello  Simple program that greets NAME for a total of COUNT times. view as the main view of application

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <layout>
+    <DateWithAssignment v-for="i in 10" :key="i"></DateWithAssignment>
+  </layout>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Layout from "@/layouts/Main.vue";
+import DateWithAssignment from "@/components/DateWithAssignment.vue";
+
+export default Vue.extend({
+  name: "Home",
+  components: {
+    Layout,
+    DateWithAssignment,
+  },
+});
+</script>
