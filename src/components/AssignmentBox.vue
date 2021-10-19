c0f7b12226166844799e9a5266a7bb9c5b9cd195
Poomtum <poomtum2544@hotmail.com>
2021-10-19 22:57:42 +0700

    :lipstick: Add components for assignment

---
+++ src/components/AssignmentBox.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="box">
+        <md-card class="md-primary">
+            <md-card-header>
+                <div class="md-title assignment-content">Read about Agile</div>
+            </md-card-header>
+            <md-card-content>
+                <div class="md-subhead assignment-content">Read #2, #4, #16 tips</div>
+            </md-card-content>
+
+
+        </md-card>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class AssignmentBox extends Vue {
+}
+</script>
+
+<style scoped>
+.assignment-content {
+    text-align: left;
+}
+
+.box {
+    margin: 30px 0px;
+}
+
+</style>
