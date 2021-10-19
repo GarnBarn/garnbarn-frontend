01565444fa502aaa1f94d95767f6f94273f102d6
Siratee K <sirateek@gmail.com>
2021-10-19 20:19:03 +0700

    Add vue-material

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueMaterial from "vue-material";
+import "vue-material/dist/vue-material.min.css";
+
+Vue.use(VueMaterial);
 
 Vue.config.productionTip = false;
 
