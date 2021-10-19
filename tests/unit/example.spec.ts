d38b8b10373aad2d846d6361add21533897a49e0
Siratee K <sirateek@gmail.com>
2021-10-20 00:10:45 +0700

    Remove the test file and add the sample test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,5 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
-
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+describe("Example Test File", () => {
+  it("Example test", () => {
+    expect(1).toEqual(1);
   });
 });
