eccb80c92be4bc0c64044cce2bde133c9fc4b921
Siratee K <sirateek@gmail.com>
2021-10-23 18:05:11 +0700

    Add SignIn Component and add /signIn path

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,23 @@ import App from "./App.vue";
 import router from "./router";
 import VueMaterial from "vue-material";
 import "vue-material/dist/vue-material.min.css";
+import * as firebase from "firebase/app";
+import axios, { AxiosResponse } from "axios";
+
+let firebaseConfig: any;
+const DEVELOPMENT_MODE = process.env.NODE_ENV;
+console.log(process.env);
+if (DEVELOPMENT_MODE === "development") {
+  // In Development Mode, We should get the credential from Local File.
+  firebaseConfig = require("@/firebaseConfig.json");
+  firebase.initializeApp(firebaseConfig);
+} else {
+  // In GarnBarn Production Environment, We can fetch the credential
+  // directly from Firebase Hosting.
+  axios.get("/__/firebase/init.json").then((response: AxiosResponse) => {
+    firebase.initializeApp(JSON.parse(response.data as string));
+  });
+}
 
 Vue.use(VueMaterial);
 
---
+++ src/views/SignIn.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="signInBox">
+    <md-card class="signInCard">
+      <md-card-media-actions>
+        <md-card-media>
+          <img src="@/assets/garnbarn-logo.png" class="garnbarnLogo" />
+        </md-card-media>
+
+        <md-card-actions>
+          <h3>Sign in to GarnBarn</h3>
+          <section id="firebaseui-auth-container"></section>
+        </md-card-actions>
+      </md-card-media-actions>
+    </md-card>
+    <!-- <md-card class="signInCard">
+      <img src="@/assets/garnbarn-logo.png" class="garnbarnLogo" />
+      <hr class="rounded" />
+      <h3>Sign in to GarnBarn</h3>
+      <section id="firebaseui-auth-container"></section>
+    </md-card> -->
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { getAuth, Auth, GoogleAuthProvider } from "firebase/auth";
+import * as firebaseUi from "firebaseui";
+import "firebaseui/dist/firebaseui.css";
+
+@Component
+export default class SignIn extends Vue {
+  firebaseAuthInstance: Auth = getAuth();
+
+  beforeMount() {
+    let uiConfig: any = {
+      signInSuccessUrl: "/home",
+      signInOptions: [GoogleAuthProvider.PROVIDER_ID],
+    };
+    console.log("Here");
+    let ui = new firebaseUi.auth.AuthUI(this.firebaseAuthInstance);
+    ui.start("#firebaseui-auth-container", uiConfig);
+  }
+}
+</script>
+
+<style scoped>
+.signInBox {
+  padding-top: 5%;
+  /* padding-left: 30%;
+  padding-right: 30%; */
+  padding-top: 5%;
+}
+
+.signInCard {
+  padding: 10px;
+  padding-top: 15px;
+  border-radius: 20px;
+  background-color: #f9f9f9;
+  display: inline-block;
+  vertical-align: top;
+  overflow: auto;
+  border: 1px solid rgba(#000, 0.12);
+}
+
+.garnbarnLogo {
+  width: 150px;
+  height: auto;
+}
+
+hr.rounded {
+  margin-top: 5%;
+  margin-bottom: 5%;
+  margin-left: 10%;
+  margin-right: 10%;
+  border-top: 2px solid #bbb;
+  border-radius: 5px;
+}
+</style>
