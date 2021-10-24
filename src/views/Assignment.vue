602d0bad8ff405e23da99e2f8c51022ffadb458a
Siratee K <sirateek@gmail.com>
2021-10-25 00:04:35 +0700

    Add Assignment view and add router /assignment/:id

---
+++ src/views/Assignment.vue
@@ -0,0 +1,24 @@
+<template>
+  <layout>
+    <div>
+      <p>Assignment ID to fetch: {{ assignmentId }}</p>
+      <router-link to="/home">
+        <md-button class="md-primary md-raised">Back</md-button>
+      </router-link>
+    </div>
+  </layout>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Layout from "@/layouts/Main.vue";
+
+@Component({
+  components: {
+    Layout,
+  },
+})
+export default class AssignmentView extends Vue {
+  assignmentId: string = this.$route.params.id;
+}
+</script>
