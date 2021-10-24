ac21a95cc7ee41ef2abdc274f8b64b561deb057d
Siratee K <sirateek@gmail.com>
2021-10-24 16:18:08 +0700

    Fix prop data empty bug in TagBox componet

--- src/components/TagBox.vue
+++ src/components/TagBox.vue
@@ -1,27 +1,39 @@
 <template>
-    <div style={background-color: {{ tag.color }}} class="box">
-        <p>{{ tag.id }}</p>
-    </div>
+  <div>
+    <md-card class="tag-box" :style="`--background-color: ${tagColor};`">
+      <div class="tag-box-content h2">{{ tag.name }}</div>
+    </md-card>
+  </div>
 </template>
 
 <script lang="ts">
-import { Prop, Vue } from "vue-property-decorator";
+import { Component, Prop, Vue } from "vue-property-decorator";
 import { Tag } from "@/types/garnbarn/Tag";
 
 // binded value are at $attrs not tag prop.
 // TODO: set inheritattrs: false
+@Component
 export default class TagBox extends Vue {
-    @Prop({ required: true }) readonly tag!: Tag;
+  @Prop({ required: true }) readonly tag!: Tag;
+  tagColor = "#f9f9f9";
+
+  beforeMount() {
+    if (this.tag.color) {
+      this.tagColor = this.tag.color;
+    }
+  }
 }
 </script>
 
 <style scoped>
-.box {
-    display: flex;
-    justify-self: center;
-    width: 80px;
-    height: 50px;
-    margin: 0px 20px;
-    border-radius: 20px;
+.tag-box {
+  width: fit-content;
+  border-radius: 20px;
+  background-color: var(--background-color);
+}
+
+.tag-box-content {
+  padding: 10px;
+  color: #ffffff;
 }
 </style>
