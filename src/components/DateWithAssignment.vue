73633ff7d6db73f329d549429377ab43351ed40b
Poomtum <poomtum2544@hotmail.com>
2021-10-24 20:36:34 +0700

    Change display flex to grid

--- src/components/DateWithAssignment.vue
+++ src/components/DateWithAssignment.vue
@@ -3,11 +3,13 @@
     <div class="date">
       <h1>{{ dateString }}</h1>
     </div>
-    <assignment-box
-      v-for="assignment in assignments"
-      :key="assignments.indexOf(assignment)"
-      :assignment="assignment"
-    ></assignment-box>
+    <div class="grid">
+      <assignment-box
+        v-for="assignment in assignments"
+        :key="assignments.indexOf(assignment)"
+        :assignment="assignment"
+      ></assignment-box>
+    </div>
   </div>
 </template>
 
@@ -32,6 +34,12 @@ export default class DateWithAssignment extends Vue {
   flex: 1 1 0%;
   flex-direction: column;
   padding: 30px 0px 0px 30px;
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  gap: 0.125rem;
 }
 
 .date {
