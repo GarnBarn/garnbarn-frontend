873bc36c9163467aa3e7e572e03adc1fcfde0aac
Siratee K <sirateek@gmail.com>
2021-10-25 00:58:30 +0700

    Remove try catch and force import the firebaseConfig.json file

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,13 @@ import VueMaterial from "vue-material";
 import "vue-material/dist/vue-material.min.css";
 import "@/assets/css/light-theme.css";
 import firebase from "firebase";
-import axios, { AxiosResponse } from "axios";
 
 Vue.use(VueMaterial);
-let firebaseConfig: firebase.app.App;
-try {
-  // If the file firebaseConfig.json is found. Use the config from local.
-  firebaseConfig = require("@/firebaseConfig.json");
-  firebase.initializeApp(firebaseConfig);
-} catch (e) {
-  // If config file not found. Fetch it from GarnBarn Production Environment.
-  // We can fetch it directly from Firebase Hosting.
-  axios.get("/__/firebase/init.json").then((response: AxiosResponse) => {
-    firebase.initializeApp(response.data as firebase.app.App);
-  });
-}
+let firebaseConfig: any;
+// Please create the file firebaseConfig.json that contain
+// the firebase config. You can get it from firebase project.
+firebaseConfig = require("@/firebaseConfig.json");
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false;
 
