2806bf8a17a090d572174ac3c1bda911b73fd78f
Poomtum <poomtum2544@hotmail.com>
2021-11-01 01:19:07 +0700

    :hammer: Add condition to swap between edit and detail page

--- src/views/Assignment.vue
+++ src/views/Assignment.vue
@@ -1,11 +1,19 @@
 <template>
   <layout>
     <div>
-      <AssignmentDetail :assignment=assignment></AssignmentDetail>
-      <md-button class="md-primary md-raised">Mark As Done</md-button>
-      <router-link to="/home">
-        <md-button class="md-primary md-raised">Back</md-button>
-      </router-link>
+      <div v-if="editing" class="edit">
+        <AssignmentEdit ></AssignmentEdit>
+        <md-button @click="detail" class="md-primary md-raised">Done</md-button>
+        <md-button @click="detail" class="md-primary md-raised">Back</md-button>
+      </div>
+      <div v-else class="detail">
+        <AssignmentDetail :assignment=assignment></AssignmentDetail>
+        <md-button class="md-primary md-raised">Mark As Done</md-button>
+        <md-button class="md-primary md-raised" v-on:click="edit">Edit</md-button>
+        <router-link to="/home">
+          <md-button class="md-primary md-raised">Back</md-button>
+        </router-link>
+      </div>
     </div>
   </layout>
 </template>
@@ -13,16 +21,21 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import { Assignment } from "@/types/garnbarn/Assignment";
+import { DialogBox } from "@/components/DialogBox/DialogBox";
+import AssignmentEdit from "@/components/AssignmentEdit.vue";
 import AssignmentDetail from  "@/components/AssignmentDetail.vue";
 import Layout from "@/layouts/Main.vue";
 
 @Component({
   components: {
     Layout,
-    AssignmentDetail
+    AssignmentDetail,
+    AssignmentEdit
   },
 })
 export default class AssignmentView extends Vue {
+  editing = false;
+  confirmEditDialogBox = new DialogBox("confirmEditDialogBox");
   assignmentId = Number(this.$route.params.id);
   assignment: Assignment = {
     id: this.assignmentId,
@@ -33,7 +46,21 @@ export default class AssignmentView extends Vue {
       color: "#30475E",
     },
     description: "The overflow-wrap property in CSS allows you to specify that the browser can break a line of text inside the targeted element onto multiple lines in an otherwise unbreakable place. This helps to avoid an unusually long string of text causing layout problems due to overflow.",
-    dueDate: 1635336944
+    dueDate: 1635439072,
+  }
+
+  edit(): void {
+    this.editing = true;
+  }
+
+  detail(): void {
+    this.confirmEditDialogBox.show({
+        dialogBoxContent: {
+          title: "Confirm edit",
+          content: `Are you sure?`,
+        },
+    });
+    this.editing = false;
   }
 }
 </script>
