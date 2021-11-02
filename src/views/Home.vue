a431dc9910c0dae657aa666db4e958db0a2def91
Siratee K <sirateek@gmail.com>
2021-11-03 00:45:56 +0700

    Commit progress, Add empty state in case the calling api has throw and error and make the processData for assignment

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,21 @@
 <template>
   <layout :callback="callback">
-    <DateWithAssignment
-      v-for="assignmentInDay in assignments.dateWithAssignments"
-      :key="assignments.dateWithAssignments.indexOf(assignmentInDay)"
-      :dateString="assignmentInDay.date"
-      :assignments="assignmentInDay.assignments"
-    ></DateWithAssignment>
+    <div v-if="!callApiError.isError">
+      <DateWithAssignment
+        v-for="assignmentInDay in assignments.dateWithAssignments"
+        :key="assignments.dateWithAssignments.indexOf(assignmentInDay)"
+        :dateString="assignmentInDay.date"
+        :assignments="assignmentInDay.assignments"
+      ></DateWithAssignment>
+    </div>
+    <md-empty-state
+      class="md-accent"
+      md-icon="error_outline"
+      md-label="An Error occurred while calling GarnBarn API"
+      :md-description="callApiError.errorMessage"
+      v-else
+    >
+    </md-empty-state>
   </layout>
 </template>
 
@@ -13,7 +23,7 @@
 import Layout from "@/layouts/Main.vue";
 import DateWithAssignment from "@/components/DateWithAssignment.vue";
 import { Component, Vue } from "vue-property-decorator";
-import { DateWithAssignments } from "@/types/garnbarn/Assignment";
+import { Assignment, DateWithAssignments } from "@/types/garnbarn/Assignment";
 import GarnBarnApi from "@/services/GarnBarnApi/GarnBarnApi";
 import DialogBox from "@/components/DialogBox/DialogBox";
 import firebase from "firebase";
@@ -27,89 +37,92 @@ import firebase from "firebase";
 export default class Home extends Vue {
   garnBarnAPICaller: GarnBarnApi | undefined = undefined;
   informDialogBox = new DialogBox("informDialogBox");
+  loadingDialogBox = new DialogBox("loadingDialogBox");
   assignments: DateWithAssignments = {
-    dateWithAssignments: [
-      {
-        date: "12 Sep.",
-        assignments: [
-          {
-            id: 1,
-            name: "Test 1",
-            tag: {
-              id: 1,
-              name: "Test Tag",
-              color: "#30475E",
-            },
-          },
-          {
-            id: 2,
-            name: "Test 2",
-            tag: {
-              id: 2,
-              name: "Tag 2",
-              color: "#ff9e59",
-            },
-          },
-        ],
-      },
-      {
-        date: "20 Oct.",
-        assignments: [
-          {
-            id: 3,
-            name: "Test 3",
-            tag: {
-              id: 3,
-              name: "Tag 3",
-              color: "#ffbf59",
-            },
-          },
-        ],
-      },
-      {
-        date: "30 Oct.",
-        assignments: [
-          {
-            id: 4,
-            name: "Test 4",
-            tag: {
-              id: 3,
-              name: "Tag 3",
-              color: "#CBAF87",
-            },
-          },
-          {
-            id: 5,
-            name: "Test 5",
-          },
-          {
-            id: 6,
-            name: "Test 6",
-          },
-          {
-            id: 7,
-            name: "Test 7",
-          },
-        ],
-      },
-    ],
+    dateWithAssignments: [],
   };
+  callApiError = {
+    isError: false,
+    errorMessage: "",
+  };
+
   callback(user: firebase.User, loadingDialogBox: DialogBox) {
     this.garnBarnAPICaller = new GarnBarnApi(user);
     this.get(loadingDialogBox);
   }
   async get(loadingDialogBox: DialogBox) {
-    try {
-      const apiResponse = await this.garnBarnAPICaller?.v1().assignment().all();
-    } catch (e) {
-      this.informDialogBox.show({
-        dialogBoxContent: {
-          title: "Error",
-          content: `Can't fetch data from GarnBarn API, Please try again or contact Administrator.`,
-        },
+    this.garnBarnAPICaller
+      ?.v1()
+      .assignment()
+      .all()
+      .then((apiResponse) => {
+        this.assignments = this.processData(
+          (apiResponse.data as any).results as Array<Assignment>
+        );
+        loadingDialogBox.dismiss();
+      })
+      .catch((e) => {
+        console.error(e);
+        this.informDialogBox.show({
+          dialogBoxContent: {
+            title: "Error",
+            content: `Can't fetch data from GarnBarn API, Please try again or contact Administrator.`,
+          },
+          dialogBoxActions: [
+            {
+              buttonContent: "Try again",
+              buttonClass: "md-primary",
+              onClick: async () => {
+                await this.informDialogBox.dismiss();
+                this.get(loadingDialogBox);
+              },
+            },
+            {
+              buttonContent: "Dismiss",
+              buttonClass: "md-secondary",
+              onClick: async () => {
+                this.callApiError.isError = true;
+                this.callApiError.errorMessage = e;
+                await loadingDialogBox.dismiss();
+                this.informDialogBox.dismiss();
+              },
+            },
+          ],
+        });
       });
-    }
-    loadingDialogBox.dismiss();
+  }
+
+  processData(assignments: Array<Assignment>): DateWithAssignments {
+    type mapPosWithData = {
+      [k: string]: number;
+    };
+    const mapDateWithPos: mapPosWithData = {};
+    const cacheDatewithAssignment: DateWithAssignments = {
+      dateWithAssignments: [],
+    };
+    assignments.forEach((item) => {
+      if (!item.dueDate) {
+        return;
+      }
+      const dueDate = new Date(item.dueDate);
+      const dateTitle = dueDate.toLocaleString("en-GB", {
+        day: "numeric",
+        month: "short",
+      });
+      if (!(dateTitle in mapDateWithPos)) {
+        // If no date found before, Append the new one
+        cacheDatewithAssignment.dateWithAssignments.push({
+          date: dateTitle,
+          assignments: [item],
+        });
+      } else {
+        const posOfDate = mapDateWithPos[dateTitle];
+        cacheDatewithAssignment.dateWithAssignments[posOfDate].assignments.push(
+          item
+        );
+      }
+    });
+    return cacheDatewithAssignment;
   }
 }
 </script>
