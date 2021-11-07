03e83a844aff72a130848d279b702881f693c943
Poomtum <poomtum2544@hotmail.com>
2021-11-07 16:40:01 +0700

    :wrench: fix onclick edit call

--- src/views/Assignments.vue
+++ src/views/Assignments.vue
@@ -4,7 +4,7 @@
       <h1>Add Assignment</h1>
       <md-button
         class="md-icon-button md-raised md-primary"
-        v-on:click="edit('assignment')"
+        v-on:click="edit"
       >
         <md-icon>add</md-icon>
       </md-button>
