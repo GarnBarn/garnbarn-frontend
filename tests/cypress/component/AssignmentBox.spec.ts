bdf092d4f70b103ec3497729cbd2c9e5295219ae
TopsonArcana <poomtum2544@hotmail.com>
2021-11-21 14:58:37 +0700

    Fix test

--- tests/cypress/component/AssignmentBox.spec.ts
+++ tests/cypress/component/AssignmentBox.spec.ts
@@ -6,13 +6,27 @@ import "@/assets/css/light-theme.css";
 import AssignmentBox from "@/components/AssignmentBox.vue";
 Vue.use(VueMaterial);
 
+const assignment = {
+  id: 1,
+  name: "Test 1",
+  author: "A",
+  reminderTime: [50, 50, 50],
+  description:
+    "The overflow-wrap property in CSS allows you to specify that the browser can break a line of text inside the targeted element onto multiple lines in an otherwise unbreakable place. This helps to avoid an unusually long string of text causing layout problems due to overflow.",
+  dueDate: 1635439072,
+};
+
 describe("Test Assignment Box", () => {
   it("Check assignment box color when there's no tag", () => {
-    mount(AssignmentBox);
+    mount(AssignmentBox, {
+      propsData:{ assignment },
+      stubs: { transition: false },
+    });
+
     cy.get(".assignment-card").should(
       "have.css",
       "background-color",
-      "#f9f9f9"
+      "rgb(249, 249, 249)"
     );
   });
 });
