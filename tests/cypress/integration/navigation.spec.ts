aeb253280ae6f5de60f35e766fadb5979fe49d47
Siratee K <sirateek@gmail.com>
2021-11-22 00:33:51 +0700

    Modify the workflow and add the UID as an env for cypress

--- tests/cypress/integration/navigation.spec.ts
+++ tests/cypress/integration/navigation.spec.ts
@@ -1,40 +1,40 @@
 describe("navigate through the web", () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:8080/home');
-        cy.login();
-    })
+  beforeEach(() => {
+    cy.visit("http://localhost:8080/home");
+    cy.login(Cypress.env().UID);
+  });
 
-    it("land at home page after login", () => {
-        cy.url().should('include', '/home');
-    })
+  it("land at home page after login", () => {
+    cy.url().should("include", "/home");
+  });
 
-    it("click assignment box", () => {
-        cy.get('.assignment-card').click();
+  it("click assignment box", () => {
+    cy.get(".assignment-card").click();
 
-        cy.url().should('include', '/assignment/');
-    })
+    cy.url().should("include", "/assignment/");
+  });
 
-    it("click assignments button", () => {
-        cy.get('.md-button-content').contains('Assignment').click();
+  it("click assignments button", () => {
+    cy.get(".md-button-content").contains("Assignment").click();
 
-        cy.url().should('include', '/assignment');
-    })
+    cy.url().should("include", "/assignment");
+  });
 
-    it("click tags button", () => {
-        cy.get('.md-button-content').contains('Tag').click();
+  it("click tags button", () => {
+    cy.get(".md-button-content").contains("Tag").click();
 
-        cy.url().should('include', '/tag');
-    })
+    cy.url().should("include", "/tag");
+  });
 
-    it("click logo", () => {
-        cy.get('.logo').click();
+  it("click logo", () => {
+    cy.get(".logo").click();
 
-        cy.url().should('include', '/home');
-    })
+    cy.url().should("include", "/home");
+  });
 
-    it("click profile image", () => {
-        cy.get('.profile-image').click();
+  it("click profile image", () => {
+    cy.get(".profile-image").click();
 
-        cy.url().should('include', '/account');
-    })
-})
+    cy.url().should("include", "/account");
+  });
+});
